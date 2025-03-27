80e318ba424d1f183d8b7fc14d38baf90f56032f
pafrany1 <185699921+pafrany1@users.noreply.github.com>
2025-03-27 09:29:11 +0100

    updated benchmark setup

--- examples/polyurethane/run.py
+++ examples/polyurethane/run.py
@@ -2,8 +2,9 @@
 
 from martini_daemon import simulation
 from martini_daemon.reporters.bond_reporter import BondReporter
+from martini_daemon.reporters.all_bond_reporter import AllBondReporter
 
 sim = simulation.DaemonSimulation("system.top", "system.gro",
-                                  reporters=[BondReporter],
-                                  max_steps=1000)
+                                  reporters=[BondReporter, AllBondReporter],
+                                  max_steps=1000, platform="CUDA")
 sim.simulate()
