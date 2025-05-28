c55fcaa0fa81bba46e3b84d4aee4b8604e7b28d8
pafrany1 <185699921+pafrany1@users.noreply.github.com>
2025-05-28 12:55:01 +0200

    atomic writes and custom freq checkpoint reporting

--- martini_daemon/reporters/checkpoint_reporter.py
+++ martini_daemon/reporters/checkpoint_reporter.py
@@ -1,22 +1,44 @@
-# the openmm part of reporting is inspired by
-# https://github.com/openmm/openmm/blob/master/wrappers/python/openmm/app/checkpointreporter.py
-
 from ..sysstar import SysStar
 from ..topstar import TopStar
 from .reporter import Reporter
 import pickle
+import os
 
 
 class CheckpointReporter(Reporter):
+    """
+        Reporter that enables simple continuable
+        simulations, by writing a checkpoint (.chk)
+        file every once in a while.
 
-    def __init__(self):
-        pass
+        Checkpoints can be used to restore DaemonSimulation
+        instances with the same T*, S*, openmm system
+        as was saved during checkpoint writing.
+
+        the openmm part of checkpoint writing is
+        inspired by the their checkpoint reporter
+        described at https://github.com/openmm/openmm/blob/master/wrappers/python/openmm/app/checkpointreporter.py
+
+        every_n_frames gives a goal checkpoint frequency (in MD steps).
+        The frequency might not be perfect -- it is only checked
+        on xtc frame writes, so every_n_frames gives
+        a lower bound on the frequency of writing
+        checkpoints.
+    """
+
+    def __init__(self, every_n_frames):
+        self.every_n_frames = every_n_frames
+        self.last_frame = 0
+        self.since_checkpoint = 0
 
     def on_xtc_frame(self, i, pos, box, xtc_name):
-        if i == 0:
+        self.since_checkpoint += i - self.last_frame
+        self.last_frame = i
+        if i == 0 or self.since_checkpoint < self.every_n_frames:
             return
         check_path = xtc_name + ".chk"
         save_checkpoint(check_path, i, box, self._topstar, self._sysstar)
+        self.since_checkpoint = 0
 
 
 def load_checkpoint(path, logger, epsilon_r, nonbonded_cutoff, nlist_cutoff):
@@ -45,15 +67,21 @@ def load_checkpoint(path, logger, epsilon_r, nonbonded_cutoff, nlist_cutoff):
 
 def save_checkpoint(path, i, box, top: TopStar, sys: SysStar):
     """
-        Saves a checkpoint file at <path>.
+        Atomically saves a checkpoint file at <path>.
 
         Saves the current frame (i), T* (top), S* (sys).
 
         Load with load_checkpoint.
     """
-    with open(path, "wb") as file:
+    # power outage or similar during checkpoint writing?
+    # we need atomic writes, we achieve this with
+    # writing a tempfile first, and then using os.replace
+    # to move the file, overwriting the old one
+    tmpfile = f"{path}#"
+    with open(tmpfile, "wb") as file:
         f = pickle.Pickler(file)
         f.dump(i)
         sys.save(f, box)
         top.save(f)
         f.dump("magic")
+    os.replace(tmpfile, path)
